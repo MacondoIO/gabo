15bcbdc5d69c76dec3b8d093a88742efb577fd9b
Andres Root <info@andresroot.co>
2018-06-10 00:35:58 -0500

    Setup project

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
-setup.py
+from distutils.core import setup
+setup(
+    name='macondo',
+    packages=['macondo'],
+    version='0.1',
+    description='A random test lib',
+    author='Andres Lujan',
+    author_email='info@andresroot.co',
+    url='https://github.com/MacondoPress/macondo/',
+    download_url='https://github.com/macondo/macondo/archive/0.1.tar.gz',
+    keywords=['cms', 'django', 'privacy'],
+    classifiers=[],
+)
